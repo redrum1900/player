ad231634fe141a4d45f879001f284cc32b3d701b
mani <mani95lisa@gmail.com>
2014-05-11 17:39:13 +0800

    change baev3

--- server.js
+++ server.js
@@ -7,17 +7,19 @@ var options = {
     'user': '9RGMgDe0USUb1ODDnQgRBhN2',
     'passwd': 'xt6e5Qrx93m1ebGHUpxHh7qB4CjnlKti'
 }
-log4js.addAppender(log4js.appenders['baev3-log'](options));
-var logger = log4js.getLogger('node-log-sdk');
-logger.setLevel('TRACE');
-logger.trace('baev3-log trace log');
-logger.debug('baev3-log Debug log');
-logger.info('baev3-log Info log');
-logger.warn('baev3-log Warn log');
-logger.error('baev3-log Error log');
-logger.fatal('baev3-log Fatal log');
 
 http.createServer(function(req, res) {
+
+	log4js.addAppender(log4js.appenders['baev3-log'](options));
+	var logger = log4js.getLogger('node-log-sdk');
+	logger.setLevel('TRACE');
+	logger.trace('baev3-log trace log');
+	logger.debug('baev3-log Debug log');
+	logger.info('baev3-log Info log');
+	logger.warn('baev3-log Warn log');
+	logger.error('baev3-log Error log');
+	logger.fatal('baev3-log Fatal log');
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
     res.end('<p>Hello World</p>');
