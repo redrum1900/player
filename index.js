67bdaf9c07838c91dd3e7ba22a344b27f6dce0ee
mani <mani95lisa@gmail.com>
2014-05-11 23:58:20 +0800

    init

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ app.configure = function configure(nconf, next) {
         }
         log4js.addAppender(log4js.appenders['baev3-log'](options));
         var logger = log4js.getLogger('node-log-sdk');
-        logger.trace('Startup trace log11');
-        logger.debug('Startup Debug log1');
-        logger.info('Startup Info log1');
-        logger.warn('Startup Warn log1');
-        logger.error('Startup Error log1');
-        logger.fatal('Startup Fatal log1');
+        logger.trace('baev3-log trace log11');
+        logger.debug('baev3-log Debug log1');
+        logger.info('baev3-log Info log1');
+        logger.warn('baev3-log Warn log1');
+        logger.error('baev3-log Error log1');
+        logger.fatal('baev3-log Fatal log1');
     }
 
     var u1 = new Manager({
