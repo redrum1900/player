00b9f93eaf98eb0cc1229962d8f3b191af600057
mani <mani95lisa@gmail.com>
2014-05-11 16:39:10 +0800

    add log

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var http = require('http');
 var port = 18080;
+
+var log4js = require('log4js');
+log4js.loadAppender('baev3-log');
+var options = {
+    'user': '9RGMgDe0USUb1ODDnQgRBhN2',
+    'passwd': 'xt6e5Qrx93m1ebGHUpxHh7qB4CjnlKti'
+}
+log4js.addAppender(log4js.appenders['baev3-log'](options));
+var logger = log4js.getLogger('node-log-sdk');
+logger.setLevel('TRACE');
+logger.trace('baev3-log trace log');
+logger.debug('baev3-log Debug log');
+logger.info('baev3-log Info log');
+logger.warn('baev3-log Warn log');
+logger.error('baev3-log Error log');
+logger.fatal('baev3-log Fatal log');
+
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
